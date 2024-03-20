c2a14a8fae400f21b54433ae8e3ad7fcb2a7e0ff
teodora-marinescu <147584317+teodora-marinescu@users.noreply.github.com>
2024-03-20 22:27:17 +0200

    Update rng.py

--- rng.py
+++ rng.py
@@ -1,12 +1,20 @@
 from random import random
+
+# generates random number
 a=int(random()*1000)
+# number of tries is set to 0
 t=0
+# q is the number you guess
 q=int(input("guess : "))
+
+# while your guess is wrong the number of tries increases 
 while q!=a :
     if a<q : print ("lower")
     else : print ("higher")
     q=int(input("guess : "))
     t+=1
+
+# when the guess is correct the message and number of tries are displayed
 print("CORRECT")
-print("guesses : " +str(t))
+print("guesses : ",t)
 k=int(input())
